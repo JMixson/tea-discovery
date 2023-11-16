8c7e592fc2031076626e15538c57d0e3e2d24b63
Jasmine Mixson <jmixson1179@gmail.com>
2023-11-15 23:50:57 -0800

    Move HomePage content and api fetch into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-import HomePage from './pages/HomePage';
+import { useState, useEffect } from 'react';
+import { Container } from 'reactstrap';
+import TeasList from '../src/features/TeasList';
+import TeaDetail from '../src/features/TeaDetail';
+import TeaButton from '../src/features/TeaButton';
+
+const request = 'https://boonakitea.cyclic.app/api/all';
 
 function App() {
+  const [teas, setTeas] = useState(null);
+
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch(request);
+      const fetchedTeas = await response.json();
+      setTeas(fetchedTeas);
+    }
+    getData();
+  }, []);
+
   return (
-    <>
-      <HomePage />
-    </>
+    <Container className="my-5">
+      <TeaDetail teas={teas} index={5} />
+      <TeaButton />
+      <TeasList teas={teas} />
+    </Container>
   );
 }
 

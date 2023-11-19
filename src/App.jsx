25e31abda8d2b83547cc3a2fe399e6fd67ebc836
Jasmine Mixson <jmixson1179@gmail.com>
2023-11-18 22:44:35 -0800

    Create routes to pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,14 @@
-import { useState, useEffect } from 'react';
-import { Container, Button } from 'reactstrap';
-import TeasList from '../src/features/TeasList';
-import TeaDetail from '../src/features/TeaDetail';
-
-const request = 'https://boonakitea.cyclic.app/api/all';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import TeaDetailPage from './pages/TeaDetailPage';
 
 function App() {
-  const [teas, setTeas] = useState(null);
-  const [randomIndex, setRandomIndex] = useState(0);
-
-  useEffect(() => {
-    async function getData() {
-      const response = await fetch(request);
-      const fetchedTeas = await response.json();
-      setTeas(fetchedTeas);
-    }
-    getData();
-  }, []);
-
-  function getRandomIndex() {
-    setRandomIndex(Math.floor(Math.random() * 42));
-  }
-
   return (
-    <Container className="my-5">
-      <TeaDetail teas={teas} index={randomIndex} />
-
-      <div className="d-flex">
-        <Button onClick={getRandomIndex} className="my-4 mx-auto py-2 border-0">
-          Discover a New Tea
-        </Button>
-      </div>
-
-      <TeasList teas={teas} />
-    </Container>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/:index" element={<TeaDetailPage />} />
+      <Route path="*" element={<HomePage />} />
+    </Routes>
   );
 }
 

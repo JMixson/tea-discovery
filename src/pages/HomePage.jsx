91b07dca7121b910b6cf7bcdbcb64adc829b1057
Jasmine Mixson <jmixson1179@gmail.com>
2023-11-19 13:01:41 -0800

    Add conditional logic to change what is first displayed

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,6 +9,7 @@ const request = 'https://boonakitea.cyclic.app/api/all';
 function HomePage() {
   const [teas, setTeas] = useState(null);
   const [teaIndex, setTeaIndex] = useState(0);
+  const [isDisplayingTea, setIsDisplayingTea] = useState(false);
   const { index } = useParams();
 
   useEffect(() => {
@@ -23,16 +24,26 @@ function HomePage() {
   useEffect(() => {
     if (index) {
       setTeaIndex(index);
+      setIsDisplayingTea(true);
     }
   }, [index]);
 
   function getRandomIndex() {
     setTeaIndex(Math.floor(Math.random() * 42));
+    setIsDisplayingTea(true);
   }
 
   return (
     <Container className="my-5">
-      <TeaDetail teas={teas} index={teaIndex} />
+      {isDisplayingTea ? (
+        <TeaDetail teas={teas} index={teaIndex} />
+      ) : (
+        <Container className="bg-brown rounded-3 extra-padding mb-2 text-center">
+          <h1>Discover a new taste with an exciting new tea!</h1>
+          <br />
+          <i className="fa fa-arrow-down fa-2x"></i>
+        </Container>
+      )}
 
       <div className="d-flex">
         <Button onClick={getRandomIndex} className="my-4 mx-auto py-2 border-0">

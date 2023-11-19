c1630ba37c37fd21ef29ada799c25c00c1994ee2
Jasmine Mixson <jmixson1179@gmail.com>
2023-11-18 22:56:38 -0800

    Add ScrollToTop.jsx so links scroll back to top of page

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,12 @@ import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.css';
 import './index.css';
+import ScrollToTop from './components/scrollToTop.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </React.StrictMode>

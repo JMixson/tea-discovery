baa1db986e1ee127f213b0cebebac50b3e7c2f9a
Jasmine Mixson <jmixson1179@gmail.com>
2023-11-18 23:21:28 -0800

    Remove Container around TeaCards

--- src/features/TeasList.jsx
+++ src/features/TeasList.jsx
@@ -1,24 +1,22 @@
 /* eslint-disable react/jsx-key */
-import { Container, Row, Col } from 'reactstrap';
+import { Row, Col } from 'reactstrap';
 import TeaCard from './TeaCard';
 
 const TeasList = ({ teas }) => {
   let teaIndex = -1;
 
   return (
-    <Container>
-      <Row sm="2" lg="3">
-        {teas &&
-          teas.map(tea => {
-            teaIndex++;
-            return (
-              <Col>
-                <TeaCard tea={tea} key={teaIndex} teaIndex={teaIndex} />
-              </Col>
-            );
-          })}
-      </Row>
-    </Container>
+    <Row sm="2" lg="3">
+      {teas &&
+        teas.map(tea => {
+          teaIndex++;
+          return (
+            <Col>
+              <TeaCard tea={tea} key={teaIndex} teaIndex={teaIndex} />
+            </Col>
+          );
+        })}
+    </Row>
   );
 };
 

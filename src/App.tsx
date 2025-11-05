291bfadd830adda031710d73dc91e03ad2a33f35
Jasmine Mixson <jmixson1179@gmail.com>
2025-11-04 21:29:26 -0800

    Fetch tea data

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
+import { useQuery } from "@tanstack/react-query";
 import { ChevronsDown } from "lucide-react";
 
 function App() {
+  const { data, error, isLoading } = useQuery({
+    queryKey: ["teaData"],
+    queryFn: async () => {
+      const response = await fetch(
+        "https://raw.githubusercontent.com/JMixson/tea-discovery/main/public/data/teas.json",
+      );
+      return await response.json();
+    },
+  });
+
+  if (isLoading) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
+
   return (
     <div className="mt-14 flex flex-col items-center">
       <div className="bg-brown min-h-80 w-7xl content-center rounded-md">
         <h1 className="text-center text-4xl">
           Discover a new taste with an exciting new tea!
         </h1>
+        {/* <h2>{data}</h2> */}
         <ChevronsDown
           className="mx-auto mt-10 block animate-bounce"
           size={48}

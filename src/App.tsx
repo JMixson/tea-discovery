00c8b87a21005f2efc365676e7d082f0b3147f61
Jasmine Mixson <jmixson1179@gmail.com>
2025-11-07 00:00:44 -0800

    Move and import components

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,10 @@
 import { Route } from "wouter";
-import { useQuery } from "@tanstack/react-query";
-import type { Tea, TeaDetails } from "./types";
 import StarterElement from "./components/StarterElement";
 import TeaInfoElement from "./components/TeaInfoElement";
-import TeaCard from "./components/TeaCard";
 import ScrollToTop from "./components/ScrollToTop";
+import TeaList from "./components/TeaList";
 
 function App() {
-  const {
-    data: teas,
-    error,
-    isLoading,
-  } = useQuery({
-    queryKey: ["teaData"],
-    queryFn: async () => {
-      const response = await fetch(
-        "https://raw.githubusercontent.com/JMixson/tea-discovery/main/public/data/teas.json",
-      );
-      return await response.json();
-    },
-  });
-
-  if (isLoading) return "Loading...";
-
-  if (error) return "An error has occurred: " + error.message;
-
   return (
     <div className="my-14 flex flex-col items-center">
       <ScrollToTop />
@@ -32,21 +12,13 @@ function App() {
         <Route path="/" component={StarterElement} />
 
         <Route path="/tea/:id">
-          {(params) => {
-            const tea = teas.find((tea: TeaDetails) => tea.id === params.id);
-
-            return <TeaInfoElement tea={tea} />;
-          }}
+          {(params) => <TeaInfoElement id={params.id} />}
         </Route>
       </div>
 
       <button className="btn mx-auto">Discover a New Tea</button>
 
-      <section className="grid w-7xl grid-cols-4 place-items-center gap-5">
-        {teas.map((tea: Tea) => (
-          <TeaCard key={tea.id} id={tea.id} name={tea.name} image={tea.image} />
-        ))}
-      </section>
+      <TeaList />
     </div>
   );
 }

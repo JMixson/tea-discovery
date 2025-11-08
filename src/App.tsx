aa31247faa0c66a44f2c30676be44982c307b720
Jasmine Mixson <jmixson1179@gmail.com>
2025-11-07 20:53:52 -0800

    Restructure app routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
-import { Route } from "wouter";
+import { useRoute } from "wouter";
 import StarterElement from "./components/StarterElement";
 import TeaInfoElement from "./components/TeaInfoElement";
 import ScrollToTop from "./components/ScrollToTop";
 import TeaList from "./components/TeaList";
 
 function App() {
+  const [isHome] = useRoute("/");
+  const [isTeaDetails, params] = useRoute("/tea/:id");
+
   return (
     <div className="my-14 flex flex-col items-center">
       <ScrollToTop />
       <div className="bg-brown min-h-80 w-7xl content-center rounded-md">
-        <Route path="/" component={StarterElement} />
+        {isHome && <StarterElement />}
 
-        <Route path="/tea/:id">
-          {(params) => <TeaInfoElement id={params.id} />}
-        </Route>
+        {isTeaDetails && <TeaInfoElement id={params.id} />}
       </div>
 
       <button className="btn mx-auto">Discover a New Tea</button>
